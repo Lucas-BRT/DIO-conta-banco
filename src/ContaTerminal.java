43a015dea7569cf06564ec244e064459a261e2ca
lucasbrt-desktop <lucasbritodepaula.t@gmail.com>
2024-04-29 13:32:29 -0300

    commit inicial

---
+++ src/ContaTerminal.java
@@ -0,0 +1,10 @@
+public class ContaTerminal {
+    int numero;
+    String agencia;
+    String nomeCliente;
+    double saldo;
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
