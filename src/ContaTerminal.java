f0af238316d96af3b3ca7d34e2ae153377e33aba
lucasbrt-desktop <lucasbritodepaula.t@gmail.com>
2024-04-29 19:27:07 -0300

    feat: criação de conta bancária

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    int numero;
-    String agencia;
-    String nomeCliente;
-    double saldo;
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bem vindo(a) ao banco DIO!");
+
+        System.out.print("por favor, digite o número da sua conta: ");
+        long numeroConta = Long.parseLong(scanner.nextLine());
+
+        System.out.print("digite a agência incluindo o dígito: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("qual é o seu nome? ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("digite o saldo da sua conta: ");
+        float saldo = Float.parseFloat(scanner.nextLine());
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, numeroConta, saldo);
+
     }
+
 }
